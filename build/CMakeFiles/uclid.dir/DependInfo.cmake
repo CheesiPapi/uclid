28a8fe66e6956efd6aea71e6b10760873966ab79
Ethan <esmoss17@outlook.com>
2026-09-16 17:12:49 -0400

    I'm trying to set up raylib and to do that I was going to have CMake import/install, whatever its called, do it for me. Some of my code, I think Vector3.h, is giving a lot of errors. I'll have to fix that sometime, so that CMake will actually work.

---
+++ build/CMakeFiles/uclid.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/esmoss/Desktop/sdcard-link/uclid/src/World.cpp" "CMakeFiles/uclid.dir/src/World.cpp.o" "gcc" "CMakeFiles/uclid.dir/src/World.cpp.o.d"
+  "/home/esmoss/Desktop/sdcard-link/uclid/src/main.cpp" "CMakeFiles/uclid.dir/src/main.cpp.o" "gcc" "CMakeFiles/uclid.dir/src/main.cpp.o.d"
+  "" "uclid" "gcc" "CMakeFiles/uclid.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
